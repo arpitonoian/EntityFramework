6b1f384a09bfaf943ed3c360deba66f5c632603e
Ed467016 <arpitonoian@gmail.com>
2021-01-13 23:11:09 +0400

    Use Entity Framework

--- LibraryDB/Migrations/Configuration.cs
+++ LibraryDB/Migrations/Configuration.cs
@@ -16,17 +16,41 @@
             context.Books.Add(new Book() { Name = "Introduction to Object Orient Design in C++" });
             context.Books.Add(new Book() { Name = "Contributing to Eclipse: Principles, Patterns, and Plug-ins" });
             context.Books.Add(new Book() { Name = "A Lost Age" });
+            context.Books.Add(new Book() { Name = "T-SQL Fundamentals" });
+            context.Books.Add(new Book() { Name = "T-SQL Querying" });
+            context.Books.Add(new Book() { Name = "Murach's C# 2015" });
+            context.Books.Add(new Book() { Name = "Murach'S Visual Basic 2008" });
+            context.Books.Add(new Book() { Name = "Design Patterns in .NET" });
 
+           
             context.Authors.Add(new Author { FirstName = "Erich", LastName = "Gamma" });
             context.Authors.Add(new Author { FirstName = "Ralph", LastName = "Johnson" });
             context.Authors.Add(new Author { FirstName = "Richard", LastName = "Richard" });
+            context.Authors.Add(new Author { FirstName = "Anne", LastName = "Boehm" });
+            context.Authors.Add(new Author { FirstName = "Joel", LastName = "Murach" });
+            context.Authors.Add(new Author { FirstName = "Itzik", LastName = "Ben-Gan" });
+            context.Authors.Add(new Author { FirstName = "Adam", LastName = "Machanic" });
+            context.Authors.Add(new Author { FirstName = "Kevin", LastName = "Farlee" });
+            context.Authors.Add(new Author { FirstName = "Dmitri", LastName = "Nesteruk" });
 
-            context.BookAuthors.Add(new BookAuthor() { AuthorId = 1, BookId = 1 });
-            context.BookAuthors.Add(new BookAuthor() { AuthorId = 1, BookId = 2 });
-            context.BookAuthors.Add(new BookAuthor() { AuthorId = 1, BookId = 4 });
-            context.BookAuthors.Add(new BookAuthor() { AuthorId = 2, BookId = 1 });
-            context.BookAuthors.Add(new BookAuthor() { AuthorId = 3, BookId = 1 });
-            context.BookAuthors.Add(new BookAuthor() { AuthorId = 3, BookId = 2 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 1, AuthorId = 1 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 1, AuthorId = 2 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 1, AuthorId = 3 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 2, AuthorId = 1 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 2, AuthorId = 3 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 3, AuthorId = 1 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 4, AuthorId = 2 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 4, AuthorId = 1 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 5, AuthorId = 4 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 5, AuthorId = 7 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 6, AuthorId = 4 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 7, AuthorId = 4 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 7, AuthorId = 5 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 8, AuthorId = 8 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 8, AuthorId = 7 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 8, AuthorId = 6 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 9, AuthorId = 9 });
+            context.BookAuthors.Add(new BookAuthor() { BookId = 9, AuthorId = 6 });
         }
     }
 }
--- LibraryDB/Program.cs
+++ LibraryDB/Program.cs
@@ -4,24 +4,36 @@ using System.Linq;
 namespace LibraryDB
 {
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Book Name:");
             var input = Console.ReadLine();
 
             using (var context = new LibraryContext())
             {
-             var quar = context.Books.Join(context.BookAuthors, 
-                      b1 => b1.Id,
-                      b2 => b2.BookId,
-                      (b1, b2) => new { b1, b2 }).Join(context.Authors,
-                      a => a.b2.AuthorId,
-                      b => b.Id,
-                      (a, b) => new { a, b }).Where(nam => nam.b.FirstName == input).ToList();
-
-                foreach (var a in quar)
                 {
-                    Console.WriteLine(a.a.b1.Name);
+                    var query = context.Books.Join(context.BookAuthors,
+                             b1 => b1.Id,
+                             b2 => b2.BookId,
+                             (b1, b2) => new { b1, b2 }).Join(context.Authors,
+                             a => a.b2.AuthorId,
+                             b => b.Id,
+                             (b, a) => new { b, a }).Where(nam => nam.b.b1.Name.StartsWith(input)).ToList();
+
+                    var t = 0;
+                    foreach (var q in query)
+                    {
+
+                        if (t != q.b.b1.Id)
+                        {
+                            Console.WriteLine();
+                            Console.Write(q.b.b1.Name + ": ");
+                        }
+
+                        Console.WriteLine(q.a.FirstName + " " + q.a.LastName);
+                        t = q.b.b1.Id;
+                    }
                 }
             }
         }
